c7ff5f861a9bff4d3f05e7054141a1b7f4d323e9
Rafael <rafaelfreba@gmail.com>
2024-07-30 16:44:33 -0400

    Finalizando o jogo

--- sketch.js
+++ sketch.js
@@ -1,3 +1,13 @@
+let bolaImagem;
+let jogadorImagem;
+let computadorImagem;
+let fundoImagem;
+let colisao;
+let ponto;
+let pontosJogador = 0;
+let pontosComputador = 0;
+
+
 class Raquete {
     constructor(x) {
         this.x = x;
@@ -30,15 +40,21 @@ class Raquete {
     }
 
     desenha() {
-        fill(color(0, 0, 255))
-        rect(this.x, this.y, this.w, this.h);
+        //se a raquete é o jogador 
+        if (this.x < width / 2) {
+            image(jogadorImagem, this.x, this.y, this.w, this.h)
+        } else {
+            image(computadorImagem, this.x, this.y, this.w, this.h)
+        }
     }
 }
 
 class Bola {
     constructor() {
-        this.r = 25;
+        this.r = 12;
         this.reset();
+        this.angulo = 0;
+
     }
 
     reset() {
@@ -47,13 +63,23 @@ class Bola {
         const velocidadeMaxima = 5;
         this.vx = Math.random() * velocidadeMaxima * 2 - velocidadeMaxima;
         this.vy = Math.random() * velocidadeMaxima * 2 - velocidadeMaxima;
+        this.angulo = 0;
     }
 
     update() {
         this.x += this.vx;
         this.y += this.vy;
+        this.angulo += Math.sqrt(this.vx * this.vx + this.vy * this.vy) / 30;
+
         if (this.x < this.r || this.x > width - this.r) {
-            this.reset();
+            if(this.x < this.r){
+                pontosComputador++;
+            } else {
+                pontosJogador++;
+            }
+            golSom.play();
+            falaPontos();
+            this.reset();            
         }
         if (this.y < this.r || this.y > height - this.r) {
             this.vy *= -1;
@@ -61,6 +87,7 @@ class Bola {
 
         if (colideRetanguloCirculo(this.x, this.y, this.r, jogador.x, jogador.y, jogador.w, jogador.h) ||
             colideRetanguloCirculo(this.x, this.y, this.r, computador.x, computador.y, computador.w, computador.h)) {
+            quicarSom.play();
             this.vx *= -1;
             this.vx *= 1.1;
             this.vy *= 1.1;
@@ -69,8 +96,12 @@ class Bola {
     }
 
     desenha() {
-        fill(color(255, 0, 0))
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+        push();
+        translate(this.x, this.y);
+        rotate(this.angulo);
+        imageMode(CENTER);
+        image(bolaImagem, 0, 0, this.r * 2, this.r * 2);
+        pop();
     }
 }
 
@@ -92,6 +123,22 @@ let bola;
 let jogador;
 let computador;
 
+function falaPontos(){
+    const synth = window.speechSynthesis;
+    const texto = `Pontuação: ${pontosJogador} a ${pontosComputador}`;
+    const utterance = new SpeechSynthesisUtterance(texto);
+    synth.speak(utterance);  
+}
+
+function preload(){
+    bolaImagem = loadImage('bola.png');
+    jogadorImagem = loadImage('barra01.png');
+    computadorImagem = loadImage('barra02.png');
+    fundoImagem = loadImage('fundo1.png');
+    quicarSom = loadSound('colisao.wav');
+    golSom = loadSound('ponto.wav');
+}
+
 function setup() {
     createCanvas(800, 400);
     bola = new Bola();
@@ -100,7 +147,18 @@ function setup() {
 }
 
 function draw() {
-    background(color(0, 0, 0));
+    let canvasAspectRatio = width / height;
+    let fundoAspectRatio = fundoImagem.width / fundoImagem.height;
+    let zoom = 1;
+    if (canvasAspectRatio > fundoAspectRatio) {
+        zoom = width / fundoImagem.width;
+    } else {
+        zoom = height / fundoImagem.height;
+    }
+    let scaledWidth = fundoImagem.width * zoom;
+    let scaledHeight = fundoImagem.height * zoom;
+    image(fundoImagem, (width - scaledWidth) / 2, (height - scaledHeight) / 2, scaledWidth, scaledHeight);    
+
     bola.update();
     bola.desenha();
     jogador.update();
